1a7781fb7ed4d51ce3c50b83c7d1ecd6150a9cbb
Sblin7 <blinsabine@cy-tech.fr>
2025-04-30 14:31:46 +0200

    Fichier à part avec les noms de colonnes

---
+++ config.py
@@ -0,0 +1,4 @@
+# filepath: /Users/sabine/Documents/GitHub/Industrialisation_IA/config.py
+ORDINAL_COLUMNS = ['NB_CASERNES', 'BDTOPO_BAT_MAX_HAUTEUR', ...]  # Liste complète
+CATEGORICAL_COLUMNS = ['ACTIVIT2', 'VOCATION', ...]  # Liste complète
+NUMERIC_COLUMNS = ['DEROG13', 'DEROG14', ...]  # Liste complète
